b94a440aeb406b76a5d4f6f75ffd3024e83ada6c
gi-hagi <yuto.0308bsk@gmail.com>
2023-02-14 22:23:20 +0900

    修正１

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
   validates :birthday, presence: true
 
-  has_many :products
-  has_many :managements
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX}
+  
+  # has_many :products
+  # has_many :managements
 end
