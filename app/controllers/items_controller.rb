bb0d8ff52dc5ee50ccad416119dfa4cc39d16572
gi-hagi <yuto.0308bsk@gmail.com>
2023-02-17 23:39:47 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,10 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  private
+
+  def message_params
+    params.require(:item).permit(:, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   # has_one :management
+  has_one_attached :image
 
   validates :title, presence: true
   validates :text, presence: true
