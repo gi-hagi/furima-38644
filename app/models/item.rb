e0a50c1a4db77f8e1ad189027bad293114ef1d18
gi-hagi <yuto.0308bsk@gmail.com>
2023-02-17 21:32:35 +0900

    アクティブハッシュの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,17 @@ class Item < ApplicationRecord
   validates :area_id, presence: true
   validates :delivery_date_id, presence: true
   validates :price, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :delivery_price
+  belongs_to :area
+  belongs_to :delivery_date
+
+  validates :category, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :situation, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_price, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :area, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_date, numericality: { other_than: 1 , message: "can't be blank"}
 end
