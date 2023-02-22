1d8cca595dd0ca5b03876aa993e26f437f3952fd
gi-hagi <yuto.0308bsk@gmail.com>
2023-02-22 14:28:01 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   # has_one :management
   has_one_attached :image
 
+  validates :image, presence: true
   validates :title, presence: true
   validates :concept, presence: true
   validates :category_id, presence: true
@@ -10,7 +11,7 @@ class Item < ApplicationRecord
   validates :delivery_price_id, presence: true
   validates :area_id, presence: true
   validates :delivery_date_id, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
