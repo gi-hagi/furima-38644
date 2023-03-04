fb61e0fe6b79bc040895c98272590568ba676c58
gi-hagi <yuto.0308bsk@gmail.com>
2023-03-04 14:50:22 +0900

    viewへの入力訂正の追加

--- app/controllers/managements_controller.rb
+++ app/controllers/managements_controller.rb
@@ -1,4 +1,6 @@
 class ManagementsController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+
   def index
     @management_form = ManagementForm.new
     @item = Item.find(params[:item_id])
