8c350461d092314e9d9f931c1bab666e9572e71e
gi-hagi <yuto.0308bsk@gmail.com>
2023-03-11 14:23:05 +0900

    トークン化

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -10,14 +10,16 @@ const pay = () => {
   cvcElement.mount('#cvc-form');
 
   const submit = document.getElementById("button");
-  
+
   submit.addEventListener("click", (e) => {
     e.preventDefault();
     payjp.createToken(numberElement).then(function (response) {
       if (response.error) {
       } else {
         const token = response.id;
-        console.log(token)
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
     });
   });
