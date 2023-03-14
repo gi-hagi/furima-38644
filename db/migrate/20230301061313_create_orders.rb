3926cbf7d93805329644cdbe8a009783271345b8
gi-hagi <yuto.0308bsk@gmail.com>
2023-03-14 21:40:24 +0900

    テストコードの修正

--- db/migrate/20230301061313_create_orders.rb
+++ db/migrate/20230301061313_create_orders.rb
@@ -6,7 +6,7 @@ class CreateOrders < ActiveRecord::Migration[6.0]
       t.integer     :area_id,     null: false
       t.string      :city,        null: false
       t.string      :address,      null: false
-      t.string      :build,       null: false
+      t.string      :build
       t.string      :phone,       null: false
       t.references  :management,  null: false, foreign_key: true
       t.timestamps
